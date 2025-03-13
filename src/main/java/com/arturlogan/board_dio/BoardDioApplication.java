d02df90b6f9413fda2c4aa0013e0dc278b6cdb5b
Artur Logan <arturlogan1ferreira@gmail.com>
2025-03-13 14:19:09 -0300

    Primeiro commit

---
+++ src/main/java/com/arturlogan/board_dio/BoardDioApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.board_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturlogan/board_dio/BoardDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturlogan.board_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
